9586bd701a40547984468a8000ee6de65b1a7e19
rakesh-gitid <rakesh_raj@rediffmail.com>
2021-07-20 18:30:29 +0530

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t rakesh-gitid/multi-client:latest rakesh-gitid/multi-client:$SHA  -f ./client/Dockerfile ./client
-docker build -t rakesh-gitid/multi-server:latest rakesh-gitid/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t rakesh-gitid/multi-worker:latest rakesh-gitid/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push rakesh-gitid/multi-client:latest
-docker push rakesh-gitid/multi-server:latest
-docker push rakesh-gitid/multi-worker:latest
-docker push rakesh-gitid/multi-client:$SHA
-docker push rakesh-gitid/multi-server:$SHA
-docker push rakesh-gitid/multi-worker:$SHA
+docker build -t rakesh0docker/multi-client:latest rakesh0docker/multi-client:$SHA  -f ./client/Dockerfile ./client
+docker build -t rakesh0docker/multi-server:latest rakesh0docker/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rakesh0docker/multi-worker:latest rakesh0docker/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push rakesh0docker/multi-client:latest
+docker push rakesh0docker/multi-server:latest
+docker push rakesh0docker/multi-worker:latest
+docker push rakesh0docker/multi-client:$SHA
+docker push rakesh0docker/multi-server:$SHA
+docker push rakesh0docker/multi-worker:$SHA
 kubectl apply -f k8s/
-kubectl set image deployments/server-deployment server=rakesh-gitid/multi-server:$SHA
-kubectl set image deployments/client-deployment client=rakesh-gitid/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=rakesh-gitid/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=rakesh0docker/multi-server:$SHA
+kubectl set image deployments/client-deployment client=rakesh0docker/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=rakesh0docker/multi-worker:$SHA
